07cc79005de0313f5e265c01cb8a905134b110b8
keshawn0927 <keshawn1.ross@famu.edu>
2026-04-07 19:39:45 -0400

    UserRepository and Usermodel

--- src/main/java/travel/exploration/local/repository/UserRepository.java
+++ src/main/java/travel/exploration/local/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package travel.exploration.local.repository;
 
+import travel.exploration.local.model.User;
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository {
+
+    User save(User user);
+
+    List<User> findAll();
+
+    Optional<User> findById(Long id);
+
+    void deleteById(Long id);
 }
