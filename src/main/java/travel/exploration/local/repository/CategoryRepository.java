9dd2366d65d8ec50b9d9d33833d6bcc38932ba3e
Jarrett0621 <jarrettmiles0621@gmail.com>
2026-04-07 21:22:02 -0400

    Update CategoryRepository.java

--- src/main/java/travel/exploration/local/repository/CategoryRepository.java
+++ src/main/java/travel/exploration/local/repository/CategoryRepository.java
@@ -1,4 +1,13 @@
 package travel.exploration.local.repository;
 
+import travel.exploration.local.model.Category;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryRepository {
+    Category save(Category category);
+    List<Category> findAll();
+    Optional<Category> findById(Long id);
+    void deleteById(Long id);
 }
