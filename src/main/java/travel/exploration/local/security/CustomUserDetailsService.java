8c5340615a732d18c3fa71daf9295d1c5b989665
Jarrett0621 <jarrettmiles0621@gmail.com>
2026-04-22 20:09:06 -0400

    Create CustomUserDetailsService.java

---
+++ src/main/java/travel/exploration/local/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package travel.exploration.local.security;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import travel.exploration.local.model.User;
+import travel.exploration.local.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(usernameOrEmail)
+                .or(() -> userRepository.findByEmail(usernameOrEmail))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPasswordHash(),
+                List.of(new SimpleGrantedAuthority(user.getRole()))
+        );
+    }
+}
