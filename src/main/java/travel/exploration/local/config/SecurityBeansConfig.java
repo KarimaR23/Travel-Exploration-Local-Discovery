4ee58b18a2aa861f146e67971d641d66692baa74
KarimaR23 <156832579+KarimaR23@users.noreply.github.com>
2026-04-27 14:01:55 -0400

    added authenticationManager

--- src/main/java/travel/exploration/local/config/SecurityBeansConfig.java
+++ src/main/java/travel/exploration/local/config/SecurityBeansConfig.java
@@ -2,8 +2,12 @@ package travel.exploration.local.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import travel.exploration.local.security.CustomUserDetailsService;
 
 @Configuration
 public class SecurityBeansConfig {
@@ -11,5 +15,15 @@ public class SecurityBeansConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            CustomUserDetailsService customUserDetailsService,
+            PasswordEncoder passwordEncoder
+    ) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(customUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(provider);
     }
 }
