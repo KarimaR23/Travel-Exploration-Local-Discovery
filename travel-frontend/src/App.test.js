30f3c9f18513b6598942d350fc6fb0e3ee2ef75b
KarimaR23 <156832579+KarimaR23@users.noreply.github.com>
2026-04-26 23:10:42 -0400

    added front-end and back-end tests

--- travel-frontend/src/App.test.js
+++ travel-frontend/src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App.js';
 
-test('renders learn react link', () => {
+test('renders app title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Gems Frontend/i)).toBeInTheDocument();
+});
+
+test('renders navigation links', () => {
+  render(<App />);
+  expect(screen.getByText(/Home/i)).toBeInTheDocument();
+  expect(screen.getByText(/Data Display/i)).toBeInTheDocument();
+  expect(screen.getByText(/Form Page/i)).toBeInTheDocument();
+});
+
+test('renders homepage text', () => {
+  render(<App />);
+  expect(
+      screen.getByText(/Travel Exploration Local/i)
+  ).toBeInTheDocument();
 });
